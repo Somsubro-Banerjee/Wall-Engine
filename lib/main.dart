18f33670386527d2d9afbc8184d036315085d371
lwplan123 <somsubrob@gmail.com>
2020-05-14 12:32:44 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WallEngine',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.black,
